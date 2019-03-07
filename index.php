b119b3d9b1d6afbfebb178de599e3062b557d810
frbravom <>
2019-03-06 21:49:36 -0300

    Primeros Archivos

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Texto sin programacion
+        <br>
+        <?php
+        $saludo="Mundo variable";
+        echo "Hola mundo 2";
+        echo "Hola ".$saludo;
+        ?>
+        <br>
+        Variables Globales $_SERVER
+        <pre>
+        <br>
+            <?php
+            var_dump($_SERVER);
+            ?>
+        </pre>
+        
+        
+
+    </body>
+</html>
