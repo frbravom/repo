7da38d0256c05219ad6b5f19730c78ed7e3958a7
frbravom <cetecom@LCT50420>
2019-03-06 22:13:45 -0300

    Cambios

--- index.php
+++ index.php
@@ -13,10 +13,39 @@ and open the template in the editor.
         Texto sin programacion
         <br>
         <?php
-        $saludo="Mundo variable";
+        $saludo = "Mundo variable";
         echo "Hola mundo 2";
-        echo "Hola ".$saludo;
+        echo "Hola " . $saludo;
+        echo "<br>";
         ?>
+        <?php
+        $meses["enero"] = "verano";
+        $meses["febrero"] = "verano";
+        $meses["marzo"] = "verano/otoño";
+        $meses["abril"] = "otoño";
+        $meses["mayo"] = "otoño/invierno";
+        $meses["junio"] = "invierno";
+        $meses["julio"] = "invierno";
+        $meses["agosto"] = "invierno";
+        $meses["septiembre"] = "invierno/primavera";
+        $meses["octubre"] = "primavera";
+        $meses["noviembre"] = "primavera";
+        $meses["diciembre"] = "primavera/verano";
+/*
+        foreach ($meses as $mes) {
+            echo $mes;
+            echo "<br>";
+        }
+  */      
+        foreach ($meses as $mes => $value) {
+            echo $mes."= estacion: ";
+            echo $value;
+            echo "<br>";
+        }
+        ?>
+
+
+
         <br>
         Variables Globales $_SERVER
         <pre>
@@ -25,8 +54,9 @@ and open the template in the editor.
             var_dump($_SERVER);
             ?>
         </pre>
-        
-        
+
+
+
 
     </body>
 </html>
